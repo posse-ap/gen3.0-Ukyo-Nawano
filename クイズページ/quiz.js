0ee32a412f311ee53970ca505e416a21edbf5da4
縄野佑匡 <ukyoriderdrive42107@ezweb.ne.jp>
2022-10-18 18:42:05 +0900

    レスポンシブ一旦完了しました

--- クイズページ/quiz.js
+++ クイズページ/quiz.js
@@ -1,35 +1,28 @@
 "use strict";
 
 {
-    const question = document.getElementById("quizQuiz");
-    const mainImage=document.getElementById('quizPicture');
-    // const image=document.getElementsByClassName("quizMain")
-    const choices = document.getElementsByClassName("quizAnswer");
-    
-    // const answerResultF = document.getElementsByClassName("answerResult");
-    // const answerResultT = document.getElementsByClassName("answerResultRight");
-    
-    let isAnswered;
-
+    // 全てのクイズの配列//
     const quizSet = [{
         quizNumber:0,
         q:"日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？",
         image:"../img/img-quiz01.png",
-        a: ["約28万人", "約79万人", "約183万人"],
+        a: ["約79万人", "約28万人", "約183万人"],
         n:'経済産業省 2019年3月 － IT 人材需給に関する調査',
+        t:"約79万人"
     },
     {
         quizNumber:1,
         q:"既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？",
         image:"../img/img-quiz02.png",
-        a: ["INTECH", "BIZZTECH", "X-TECH"],
-        
+        a: ["X-TECH","INTECH", "BIZZTECH"],
+        t:"X-TECH",
     },
     {
         quizNumber:2,
         q:"lotとは何の略でしょう？",
         image:"../img/img-quiz03.png",
-        a:["Internat of Things", "Integrate into Technology","Information om Tool"],
+        a:["Internet of Things", "Integrate into Technology","Information om Tool"],
+        t:"Internet of Things",
     },
     {
         quizNumber:3,
@@ -37,132 +30,95 @@
         image:"../img/img-quiz04.png",
         a:["Society5.0","CyPhy","SDGs"],
         n:"Society5.0-科学技術政策-内閣府",
+        t:"Society5.0",
     },
     {
         quizNumber:4,
         q:"イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
         image:"../img/img-quiz05.png",
         a:["Web3.0","NFT","メタバース"],
+        t:"Web3.0",
     },
     {
         quizNumber:5,
         q:"先進テクノロジー活用企業と出遅れた企業の収益の差はどれくらいあると言われているでしょう？",
         image:"../img/img-quiz06.png",
-        a:["約2倍","約5倍","約11倍"],
+        a:["約5倍","約2倍","約11倍"],
         n:"Accenture Technology Vision 2021",
+        t:"約5倍",
     }
 ];
-let currentNum = 0;
 
-const result=[
-    "約79万人","X-TECH","Integrate into Technology","Society5.0","メタバース","約5倍"
-];
-
-
+// 問題文シャッフルの関数//
+const shuffle = arrays => {
+    const array = arrays.slice();
+    for (let i = array.length - 1; i >= 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+    }
+    return array
+}
+const quizArray = shuffle(quizSet);
 
 const template=document.getElementById("js-template");
-for (let i = 0; i < quizSet.length; i++) {
+
+// 問題文をHTMLに出力//
+for (let i = 0; i < quizArray.length; i++) {
 const content=template.content.cloneNode(true);
-content.querySelector(".quizQuiz").textContent=quizSet[i].q;
-content.querySelector(".quizPicture").src=quizSet[i].image;
-content.querySelector(".answerChoiceOne").innerHTML=quizSet[i].a[0];
-content.querySelector(".answerChoiceTwo").innerHTML=quizSet[i].a[1];
-content.querySelector(".answerChoiceThree").innerHTML=quizSet[i].a[2];
-content.querySelector(".quizCite").textContent=quizSet[i].n;
-content.getElementById("answerResult").style.display ="none";
-content.getElementById("answerResultRight").style.display ="none";
+content.querySelector(".quizIcon").textContent=`Q${i+1}`;
+content.querySelector(".quizQuiz").textContent=quizArray[i].q;
+content.querySelector(".quizPicture").src=quizArray[i].image;
 
-// content.querySelector(".quizOne").setAttribute("data-quiz" ,i);
+// 選択肢のシャッフル//
+const shuffledChoices = shuffle(quizArray[i].a);
+content.querySelector(".answerChoiceOne").innerHTML=shuffledChoices[0];
+content.querySelector(".answerChoiceTwo").innerHTML=shuffledChoices[1];
+content.querySelector(".answerChoiceThree").innerHTML=shuffledChoices[2];
+
+content.querySelector(".quizCite").textContent=quizArray[i].n;
+content.querySelector(".answerText").textContent=quizArray[i].t;
+content.querySelector(".answerTextRight").textContent=quizArray[i].t;
 document.getElementById("js-content").appendChild(content);
+
+createCite(i);
 };
 
+// 1回ボタンを押すと押せなくする//
+const setDisabled = (buttons) => {
+    buttons.forEach(button => {
+        button.disabled = true;
+    })
+}
 
-// const li=document.getElementById("button1");
+// それぞれの問題で正解・不正解を判定//
 const quizBlock=document.querySelectorAll(".quizOne");
-console.log(quizBlock);
-quizBlock.forEach(function(num)
-{
-    
-        const li=num.querySelectorAll(".button");
-        console.log(li);
-        li.forEach(function(r){
-        function checkAnswer(r){
-            if(isAnswered===true){
-                return;
+const quizTrue=document.querySelectorAll(".answerResultRight");
+const quizFalse=document.querySelectorAll(".answerResultFalse");
+quizBlock.forEach(function(num, questionNum){
+    const li=num.querySelectorAll(".button");
+        li.forEach(
+            function(r){
+                r.addEventListener("click", () => {
+                setDisabled(li);
+                console.log(r.textContent);
+                console.log(questionNum);
+                if(r.textContent===quizArray[questionNum].t){
+                    r.classList.add('correct');
+                    quizTrue[questionNum].style.display="block";
+                }else{
+                    r.classList.add('correct');
+                    quizFalse[questionNum].style.display="block";
+                    }
+                    
+                })        
             }
-            isAnswered=true;
-            if(r.textContent===result[i-1]){
-                r.classList.add('correct');
-                document.getElementById('answerResultRight').style.display="block";
-                console.log("正解");
-            }else{
-                r.classList.add('correct');
-                document.getElementById('answerResult').style.display="block";
-        
-            }
-        
-        r.addEventListener("click",()=>{
-            checkAnswer(r);
-        });
-    }
+        );
+});
 
-    
-})});
-
-if(quizSet.quizNumber===1,2,4){
-    template.content.getElementById(".quizCite").style.display ="none";
-}else{
-    template.content.getElementById(".quizCite").style.display ="none";
+// 資料の表示(nがあるかどうかで判断)//
+function createCite(i){
+const quizCites=document.querySelectorAll(".quizCite");
+    quizArray[i].n ? quizCites[i].style.display = "flex" : "";
 };
 
-
-
-
-// function setQuiz(){
-//     isAnswered = false;
-//     question.textContent=quizSet[currentNum].q;
-
-//     let currentIndex=0;
-//     const mainImage=document.getElementById('quizPicture');
-//     mainImage.src=quizSet[currentIndex].image;
-
-//     note.textContent=quizSet[currentNum].n;
-
-//     quizSet[currentNum].a.forEach(choice=>{
-//         let li=document.createElement("li");
-//         li.textContent=choice;
-
-//         li.addEventListener("click",()=>{
-//             checkAnswer(li);
-//         })
-
-//         choices[currentNum].appendChild(li);
-//     });
-// }
-// setQuiz();
-
-// for(let i=0; i<1; i++){
-//     setQuiz(i);}
-
-// function createQuiz(){
-//     const quizBlock= document.getElementById('quizOne');
-//     const quizHtml=`<div class="`
-// }
-
-// const createQuizHtml = (quizItem, questionNumber) => {
-//     let answersHtml = '';
-// }
-// setQuiz.forEach
-// for(let i = 0; i <=1 ;i++) {
-//     setQuiz(i);
-    
-// };
-
-
-
-console.log(checkAnswer);
-    // for (let currentNum = 0; currentNum < quizSet.length; currentNum++) {
-    // Set(currentNum);
-    // checkAnswer(currentNum);
-    // }
 }
